e01220167678bfd72625f9b3c5317d5d0d48cc92
Chien Tran <minhchien.arashino@gmail.com>
2025-02-16 20:45:35 +0700

    Init project

---
+++ src/main/java/backend/backendbase/BackendBaseApplication.java
@@ -0,0 +1,13 @@
+package backend.backendbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBaseApplication.class, args);
+	}
+
+}
