30d3d32551c844c05dad37ee118a063b1d217cbf
Chien Tran <minhchien.arashino@gmail.com>
2025-03-17 21:31:28 +0700

    Remove file

--- src/main/java/backend/backendbase/repository/TenantPlanRepository.java
+++
@@ -1,8 +0,0 @@
-package backend.backendbase.repository;
-
-import backend.backendbase.entity.TenantPlan;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TenantPlanRepository extends BaseRepository<TenantPlan> {
-}
